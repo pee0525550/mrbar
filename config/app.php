f8f40daed318d39c8f80455417da3a5e1902f203
pee0525550 <pee0525550@users.noreply.github.com>
2026-09-15 17:41:17 +0700

    fix: select POS file before processing

--- config/app.php
+++ config/app.php
@@ -2,6 +2,6 @@
 return [
  'name'=>'MR BAR',
  'timezone'=>'Asia/Bangkok',
- 'version'=>'1.43.4',
- 'pack'=>'Clear POS Import Workflow v1.43.4',
+ 'version'=>'1.43.5',
+ 'pack'=>'Select Then Process Flow v1.43.5',
 ];
